68f0e0714aea9b177c2e85385ff359f3842f39a8
sachmahendra <123411058+sachmahendra@users.noreply.github.com>
2023-02-10 23:04:56 +0530

    initial.commit
    this is a code of properties under COLLECTION FRAMWORK topic java

---
+++ A255properties.java
@@ -0,0 +1,30 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.Properties;
+
+public class A255properties{
+public static void main(String[] args)throws Exception {
+
+    Properties p = new Properties();
+/* 
+    p.setProperty("brand", "Dell");
+    p.setProperty("processor", "17");
+    p.setProperty("OS", "Windows10");
+    p.setProperty("Model", "Latitude");
+
+   // System.out.println(p);
+
+  //  p.store(new FileOutputStream("/New folder (2)/My.txt2"),"Laptop"); // to store all the data in a file
+
+    // we can also use a .XML file to store the same data as
+     p.storeToXML(new FileOutputStream("MyData.xml"),"Laptop");
+
+     */
+
+     // WE CAN ALSO ACCESS THE DATA OF XML FILE BY USING FILEINPUTSTREAM AS BELOW
+     p.loadFromXML(new FileInputStream("MyData.xml"));
+     System.out.println(p);
+
+   
+}
+}
